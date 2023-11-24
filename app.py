5ec2dbea794dc65fe0853716787727ee37422509
gilgiv <gilgiv@gmail.com>
2023-11-24 11:32:09 +0200

    Add devide function

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ n2 = int(input('Please enter second number: '))
 sum = n1+n2
 sub = n1-n2
 mul = n1*n2
+divide = n1/n2
 
 print(f'{n1} + {n2} = {sum}')
 print(f'{n1} - {n2} = {sub}')
 print(f'{n1} * {n2} = {mul}')
+print(f'{n1} / {n2} = {divide}')
