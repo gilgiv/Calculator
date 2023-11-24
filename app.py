ff85a53443e0ed1eacedc28c933c4c44c05629f5
gilgiv <gilgiv@gmail.com>
2023-11-24 10:30:26 +0200

    Adding substract feature

--- app.py
+++ app.py
@@ -3,5 +3,7 @@ n1 = int(input('Please enter first number: '))
 n2 = int(input('Please enter second number: '))
 
 sum = n1+n2
+sub = n1-n2
 
-print (f'{n1} + {n2} = {sum}')
+print(f'{n1} + {n2} = {sum}')
+print(f'{n1} - {n2} = {sub}')
